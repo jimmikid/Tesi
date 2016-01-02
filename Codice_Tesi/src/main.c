10677147bcaa4e236e2fb5d90ff4e3de7dd9e856
jimmikid <gianmarco.stinchi@hotmail.com>
2016-01-02 21:53:59 +0100

    Alcuni Test
    Testate tutte le funzioni scritte fino a questo momento

--- Codice_Tesi/src/main.c
+++ Codice_Tesi/src/main.c
@@ -3,14 +3,31 @@
 #include <memory.h>
 #include <math.h>
 #include <matrici.h>
-int main()
+
+void main()
 {
-	
 	Matrice* A = alloca_matrice(2, 2);
-	imposta_valori(A, 0, 0, 1);
-	imposta_valori(A, 0, 1, 2);
-	imposta_valori(A, 1, 0, 3);
-	imposta_valori(A, 1, 1, 4);
-	stampa_matrice(A);
+	Matrice* B = alloca_matrice(2, 2);
+	//Matrice A
+	imposta_valori(A, 0, 0, 1); imposta_valori(A, 1, 0, 2);
+	imposta_valori(A, 0, 1, 3); imposta_valori(A, 1, 1, 4);
+
+	//Matrice B
+	imposta_valori(B, 0, 0, 4); imposta_valori(B, 1, 0, 8); 
+	imposta_valori(B, 0, 1, 6); imposta_valori(B, 1, 1, 12);
+	
+	Matrice* moltiplicazione = moltiplica_matrici(A, B);
+	printf("Le due matrici moltiplicate\n");
+	stampa_matrice(moltiplicazione);
+
+	Matrice* sommata = somma_matrici(A, B);
+	printf("\nLe due matrici sommate\n");
+	stampa_matrice(sommata);
+	libera_memoria(A);
+	libera_memoria(B);
+	libera_memoria(moltiplicazione);
+	libera_memoria(sommata);
+	printf("\n\n\n\nPremere un tasto per terminare...");
+	//SERVE SOLO PER BLOCCARE L'ESECUZIONE DEL PROGRAMMA PER VEDERE L'OUTPUT
 	getch();
 }
